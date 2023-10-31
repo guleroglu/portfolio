597be54fa3d3f34c27dc313730b6c7876ecad6d0
mstfsgdc <iletisim@mstfsgdc.com>
2023-10-31 18:49:52 +0300

    UPDATE App.jsx & ADD components/index.js

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Home } from "./Home/Home";
+export { default as WhoAmI } from "./WhoAmI/WhoAmI";
+export { default as Skills } from "./Skills/Skills";
+export { default as Contact } from "./Contact/Contact";
+export { default as Header } from "./Header/Header";
+export { default as LoadingScreen } from "./LoadingScreen/LoadingScreen";
