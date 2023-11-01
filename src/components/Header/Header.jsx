73ed133018b9729a50475ab5544c038650bd4c64
Mustafa Guleroglu <must60yyy@gmail.com>
2023-11-01 15:20:40 +0300

    updated Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,15 +3,19 @@ import Navbar from "../Navbar/Navbar";
 import ToggleButton from "../ToggleButton/ToggleButton";
 import Logo from "../../assets/Logo/logo.png";
 import { NavLink } from "react-router-dom";
+import ToggleLanguage from "../ToggleLanguage/ToggleLanguage";
 
 const Header = () => {
   return (
     <header className="header-wrapper">
-      <NavLink to="/">
-        <img className="logo" src={Logo} alt="" />
+      <NavLink>
+        <img src={Logo} alt="" className="logo" />
       </NavLink>
       <Navbar />
-      <ToggleButton />
+      <div className="toggles">
+        <ToggleButton />
+        <ToggleLanguage />
+      </div>
     </header>
   );
 };
