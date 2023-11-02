a34555fc646b95085511ece927fb3faac6f07404
Mustafa Guleroglu <must60yyy@gmail.com>
2023-11-02 17:57:28 +0300

    updated ToggleLanguage.jsx

--- src/components/ToggleLanguage/ToggleLanguage.jsx
+++ src/components/ToggleLanguage/ToggleLanguage.jsx
@@ -12,8 +12,8 @@ const ToggleLanguage = () => {
   return (
     <div>
       <select className="languageSelect" onChange={handleLanguage}>
-        <option value="tr">tr</option>
-        <option value="en">en</option>
+        <option value="tr">&#x1F1F9;&#x1F1F7;</option>
+        <option value="en">&#x1F1EC;&#x1F1E7;</option>
       </select>
     </div>
   );
