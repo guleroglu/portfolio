83a3a2ddd52723f1fae210984366a722cfc7cb49
Mustafa Guleroglu <must60yyy@gmail.com>
2023-11-02 15:47:45 +0300

    updated ContactButton

--- src/components/ContactButton/ContactButton.jsx
+++ src/components/ContactButton/ContactButton.jsx
@@ -1,10 +1,13 @@
+import { useContext } from "react";
 import "./ContactButton.css";
 import { NavLink } from "react-router-dom";
+import { GlobalContext } from "../../Context/GlobalContext";
 
 const ContactButton = () => {
+  const { selectedOption } = useContext(GlobalContext);
   return (
     <NavLink to="/contact" className="contact-button">
-      CONTACT ME
+      {selectedOption === false ? "CONTACT ME" : "İLETİŞİM"}
     </NavLink>
   );
 };
