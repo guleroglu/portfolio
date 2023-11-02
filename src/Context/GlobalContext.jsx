f3da800bc1f97196fe476ee4144000b733db3bfb
Mustafa Guleroglu <must60yyy@gmail.com>
2023-11-02 15:32:51 +0300

    updated GlobalContext.jsx

--- src/Context/GlobalContext.jsx
+++ src/Context/GlobalContext.jsx
@@ -5,13 +5,15 @@ export const GlobalContext = createContext();
 export const GlobalProvider = ({ children }) => {
   const [isHovered, setIsHovered] = useState(false);
   const [isToggle, setIsToggle] = useState(true);
-  
+  const [selectedOption, setSelectedOption] = useState(true);
 
   const value = {
     isHovered,
     setIsHovered,
     isToggle,
     setIsToggle,
+    selectedOption,
+    setSelectedOption,
   };
 
   return (
