3a63288d00203b74473e2d1c7aba514375786cdd
Mustafa Guleroglu <must60yyy@gmail.com>
2023-11-02 15:31:42 +0300

    updated ToggleLanguage

--- src/components/ToggleLanguage/ToggleLanguage.jsx
+++ src/components/ToggleLanguage/ToggleLanguage.jsx
@@ -1,9 +1,18 @@
+import { useContext } from "react";
 import "./ToggleLanguage.css";
+import { GlobalContext } from "../../Context/GlobalContext";
 
 const ToggleLanguage = () => {
+  const { selectedOption, setSelectedOption } = useContext(GlobalContext);
+
+  const handleLanguage = () => {
+    setSelectedOption(!selectedOption);
+  };
+
+  
   return (
     <div>
-      <select className="languageSelect">
+      <select className="languageSelect" onChange={handleLanguage}>
         <option value="tr">tr</option>
         <option value="en">en</option>
       </select>
