7a6c47c73ec1a17450b5c7e934957f439b4925cc
Mustafa Guleroglu <must60yyy@gmail.com>
2023-11-02 16:00:26 +0300

    updated Skills.jsx

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -5,12 +5,17 @@ import { useContext } from "react";
 import { GlobalContext } from "../../Context/GlobalContext";
 
 const Skills = () => {
-  const { isHovered, isToggle } = useContext(GlobalContext);
+  const { isHovered, isToggle, selectedOption } = useContext(GlobalContext);
   const hoveredStyle = isHovered ? { color: "#4ade80" } : {};
 
   return (
     <div className="skills-wrapper">
-      <h1 className="page-title">skills</h1>
+      {selectedOption === false ? (
+        <h1 className="page-title">skills</h1>
+      ) : (
+        <h1 className="page-title">yetenekler</h1>
+      )}
+
       <div className="skills-subtitle">
         <span
           style={hoveredStyle}
@@ -18,12 +23,22 @@ const Skills = () => {
         >
           &lt;
         </span>
-        <p
-          style={hoveredStyle}
-          className={`skills-subtitle ${!isToggle ? "light" : ""}`}
-        >
-          skills i know and have mastered
-        </p>
+        {selectedOption === false ? (
+          <p
+            style={hoveredStyle}
+            className={`skills-subtitle ${!isToggle ? "light" : ""}`}
+          >
+            skills i know and have mastered
+          </p>
+        ) : (
+          <p
+            style={hoveredStyle}
+            className={`skills-subtitle ${!isToggle ? "light" : ""}`}
+          >
+            bildiğim ve ustalaştığım beceriler
+          </p>
+        )}
+
         <span
           style={hoveredStyle}
           className={`htmltag ${!isToggle ? "light" : ""}`}
